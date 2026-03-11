d5ac86e24d4b8de1572e4ff5bc0faddbed13d54c
nishant <nishantxraghav@gmail.com>
2026-03-11 18:39:13 +0530

    adding files

--- supabase/migrations/20240102_enable_realtime.sql
+++
@@ -1,4 +0,0 @@
-ALTER PUBLICATION supabase_realtime ADD TABLE bgv_checklist_items;
-ALTER PUBLICATION supabase_realtime ADD TABLE companies;
-ALTER PUBLICATION supabase_realtime ADD TABLE bgv_cases;
-ALTER PUBLICATION supabase_realtime ADD TABLE case_documents;
